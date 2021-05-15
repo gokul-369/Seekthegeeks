a6610a0895c13772037995cf90b99f795669faee
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2021-05-15 16:19:28 +0530

    changes

--- public/src/register.php
+++ public/src/register.php
@@ -6,21 +6,27 @@ $mail=$_POST['mail'];
 $name=$_POST['uname'];
 $pass=$_POST['pwd'];
 $cpass=$_POST['cpwd'];
-if ($pass != $cpass) {
-    echo'<script>alert("Passwords dont match ")</script>';
-} 
-else
-{
-$s="select * from customer where mail = '$mail'";
+
+$s="select * from customer where username ='$name'";
 $result = mysqli_query($con,$s);
-$num = mysqli_num_rows($result);
-if ($num == 1) {
-    echo'<script>alert("user already exists or your pawwsword is wrong")</script>';
+$num=mysqli_num_rows($result);
+
+$s1="select * from customer where mail = '$mail'";
+$result1 = mysqli_query($con,$s1);
+$num1 = mysqli_num_rows($result1);
+
+if ($num1 == 1) {
+    echo'<script>alert("this user is already registered so login directly")</script>';
 }
+elseif($num==1){
+    echo'<script>alert("username taken")</script>';
+}
+elseif ($pass != $cpass) {
+    echo'<script>alert("Passwords did not match ")</script>';
+} 
 else {
     $reg="insert into customer (mail,username,password) values ('$mail','$name','$pass')";
     mysqli_query($con,$reg);
     header('location:../login.html');
-}
 }
 ?>
--- public/src/verify.php
+++ public/src/verify.php
@@ -4,12 +4,24 @@ $con = mysqli_connect('localhost','root','1234');
 mysqli_select_db( $con, 'users');
 $name=$_POST['uname'];
 $pass=$_POST['pwd'];
-$s="select * from customer where username ='$name' && password='$pass'";
+
+$s="select * from customer where username ='$name'";
 $result = mysqli_query($con,$s);
 $num=mysqli_num_rows($result);
+
+$s1="select * from customer where username ='$name' && password='$pass'";
+$result1=mysqli_query($con,$s1);
+$num1=mysqli_num_rows($result1);
+
 if($num==1){
+    if($num1==1)
+    {
     $_SESSION['uname']=$name;
     header('location:file.php');
+}
+else{
+    echo '<script>alert("pwd is inc")</script>';
+}
 }
 else{
     echo '<script>alert("user not found")</script>';
